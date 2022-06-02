1db1f64f6b52b31cbbcb82e7b76cccfc5d252f0a
sergeyk321 <93829765+sergeyk321@users.noreply.github.com>
2022-06-02 12:53:03 +0300

    Add files via upload

--- database.py
+++ database.py
@@ -1,41 +1,38 @@
 from re import X
 import psycopg2
 from psycopg2 import Error
+import time
 
 flag = True
 
 conn = psycopg2.connect(database="postgres",    
                             user="postgres",
-                            password="123456",
+                            password="1",
                             host="localhost",
                             port="5432")
 cursor = conn.cursor()
 
-X = 1
+X = str(round(time.time()))[7:]
 
 print(Error)
-for i in range(X):
-    try:
-        try:
-            cursor.execute(f'CREATE SCHEMA shema{X}')
-            cursor.execute(f'CREATE TABLE shema{X}.table{X}\
-                        (\
-                        frame varchar(32),\
-                        id varchar(32),\
-                        bbox_left varchar(32),\
-                        bbox_top varchar(32),\
-                        bbox_w varchar(32),\
-                        bbox_h varchar(32)\
-                        );')
-            conn.commit()
-        except (Exception,  Error) as error:
-            print(Error)
-        flag = False
-    except (Exception,  Error) as error:
-        print(Error)
-    finally:
-        if conn:
-            print(flag)
-            cursor.close()
-            conn.close()
-            print("Соединение с PostgreSQL закрыто")
+try:
+    cursor.execute(f'CREATE SCHEMA a{X}')
+    cursor.execute(f'CREATE TABLE a{X}.a{X}\
+                (\
+                frame varchar(32),\
+                id varchar(32),\
+                bbox_left varchar(32),\
+                bbox_top varchar(32),\
+                bbox_w varchar(32),\
+                bbox_h varchar(32)\
+                );')
+    conn.commit()
+except (Exception,  Error) as error:
+    print(Error)
+    flag = False
+finally:
+    if conn:
+        print(flag)
+        cursor.close()
+        conn.close()
+        print("Соединение с PostgreSQL закрыто")
